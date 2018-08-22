d3060728d76817157594049b459e4f867997f062
Juan José González Basilio <jjnorris31@gmail.com>
2018-08-21 23:10:23 -0500

    Create zero_sixty.js

---
+++ zero_sixty.js
@@ -0,0 +1,15 @@
+/*
+Your task is to drive your car from 1 to 60 Kilometers per hour by printing a pattern.
+A hyphen represents the speed of cars.
+one hyphen is counted as 1 kph.
+Examples
+1 ➞ -
+
+5 ➞ -----
+
+3➞ ---
+*/
+
+function Go(num) {
+	return '-'.repeat(num)
+}
