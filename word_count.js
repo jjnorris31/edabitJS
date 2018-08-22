3ae3b93cbb44efe9a94e23fbf62cd0de418b268d
Juan José González Basilio <jjnorris31@gmail.com>
2018-08-21 23:20:50 -0500

    Create word_count.js

---
+++ word_count.js
@@ -0,0 +1,13 @@
+/*
+Create a function that takes a string and returns the word count. The string will be a sentence.
+Examples
+"Just an example here move along" ➞ 6
+
+"This is a test" ➞ 4
+
+"What an easy task, right" ➞ 5
+*/
+
+function countWords(str) {
+	return (str.split(" ")).length
+}
